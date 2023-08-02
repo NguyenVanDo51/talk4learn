930aa8baa4d726ddd4ad6363e8342d3dacfa88f9
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-02 21:08:01 +0700

    Update InitialApp.tsx

--- src/components/layout/InitialApp.tsx
+++ src/components/layout/InitialApp.tsx
@@ -1,16 +1,15 @@
 'use client'
-import { darkTheme, defaultTheme } from '@/theme/themeConfig'
-import { ConfigProvider } from 'antd'
-import { FC } from 'react'
+import { useTheme } from '@/hooks/helpers/useTheme'
+import { FC, useEffect } from 'react'
 
 export const InitialApp: FC<{ children: any }> = ({ children }) => {
-  // const { theme } = useTheme()
-  const theme = 'dark'
-  // useEffect(() => {
-  //   document.body.classList.remove('dark')
-  //   document.body.classList.remove('light')
-  //   document.body.classList.add(theme)
-  // }, [theme])
+  const { theme } = useTheme()
 
-  return <ConfigProvider theme={theme === 'dark' ? darkTheme : defaultTheme}>{children}</ConfigProvider>
+  useEffect(() => {
+    document.body.classList.remove('dark')
+    document.body.classList.remove('light')
+    document.body.classList.add(theme)
+  }, [theme])
+
+  return <div>{children}</div>
 }
