9b201955913f933d960a0cdce4511cb88c409c90
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-02 21:06:14 +0700

    a

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,16 +1,14 @@
-'use client'
-
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
 export const MainHeader = () => {
-  const route = useRouter()
   return (
     <nav className="h-[46px]">
-      <ul className="flex w-full gap-8 justify-end px-10 py-2">
-        <li onClick={() => route.push('/lessons')}>Lessons</li>
+      <ul className='flex w-full gap-8 justify-end px-10 py-2'>
         <li>
-          <a href={'/chat'}> Chat</a>
+          <Link href={'/lessons'}> Lessons</Link>
+        </li>
+        <li>
+          <Link href={'/chat'}> Chat</Link>
         </li>
       </ul>
     </nav>
--- src/components/layout/InitialApp.tsx
+++ src/components/layout/InitialApp.tsx
@@ -1,17 +1,16 @@
 'use client'
-import { useTheme } from '@/hooks/helpers/useTheme'
 import { darkTheme, defaultTheme } from '@/theme/themeConfig'
 import { ConfigProvider } from 'antd'
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 
 export const InitialApp: FC<{ children: any }> = ({ children }) => {
-  const { theme } = useTheme()
-
-  useEffect(() => {
-    document.body.classList.remove('dark')
-    document.body.classList.remove('light')
-    document.body.classList.add(theme)
-  }, [theme])
+  // const { theme } = useTheme()
+  const theme = 'dark'
+  // useEffect(() => {
+  //   document.body.classList.remove('dark')
+  //   document.body.classList.remove('light')
+  //   document.body.classList.add(theme)
+  // }, [theme])
 
   return <ConfigProvider theme={theme === 'dark' ? darkTheme : defaultTheme}>{children}</ConfigProvider>
 }
