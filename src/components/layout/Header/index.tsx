70737300f7ad3d4eeb24d93821e3b6ca6d52f754
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-02 21:02:29 +0700

    Update index.tsx

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,14 +1,14 @@
-"use client"
+'use client'
 
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export const MainHeader = () => {
+  const route = useRouter()
   return (
     <nav className="h-[46px]">
-      <ul className='flex w-full gap-8 justify-end px-10 py-2'>
-        <li>
-          <a href={'/lessons'}> Lessons</a>
-        </li>
+      <ul className="flex w-full gap-8 justify-end px-10 py-2">
+        <li onClick={() => route.push('/lessons')}>Lessons</li>
         <li>
           <a href={'/chat'}> Chat</a>
         </li>
