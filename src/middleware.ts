9529580b2f6a57a1d58676301663d170f169acbc
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-09 23:35:11 +0700

    a

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log('request', request.headers.get('host'))
+  if (!request.url.startsWith('https://rangachat.com')) {
+    return new NextResponse('Ivalid Client', {
+      status: 400,
+    })
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  // matcher: ['/api/chat/:path*'],
+  matcher: ['/api/test/:path*'],
+}
