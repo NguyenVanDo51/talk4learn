7c3badec152e070c52822394058c4b33df6d4933
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-11 10:12:17 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { LoadingScreen } from '@/components/level1/Loading'
 import AIChatPage from '@/components/pages/chat'
 
 export default function Home() {
-  // return <AIChatPage />
-  return <LoadingScreen />
+  return <AIChatPage />
 }
