c4e305f759655f598846c38acb6ac5af83eb8cbb
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-11 11:30:30 +0700

    a

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const config = {
+  projectId: "c7yets",
+};
+
+module.exports = {
+  ...config,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
