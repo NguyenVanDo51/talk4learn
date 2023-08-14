b34855b6e88f0bd4aab417baf9e5e04fb6ae6375
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-14 23:22:08 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
 import Introduction from '@/components/template/introduction'
-import { getServerSession } from 'next-auth/next'
-import { redirect } from 'next/navigation'
-import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home(props: any) {
-  console.log('props', props)
-  // if (session) {
-  //   redirect('/app')
-  // }
+export default function Home() {
+
   return <Introduction />
 }
 
-export async function getServerSideProps(context: any) {
-  // const session = await getServerSession(context.req, context.res, authOptions)
-  const session = await getServerSession(authOptions)
-  console.log('session', session)
-
-  return {
-    props: {
-      session,
-    },
-  }
-}
