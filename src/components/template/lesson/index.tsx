8bd4d5865009372e74be2d802930c879284db392
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-08-16 22:50:33 +0700

    Update index.tsx

--- src/components/template/lesson/index.tsx
+++ src/components/template/lesson/index.tsx
@@ -1,8 +1,4 @@
-import { LessonService } from '@/service/lesson/index.service'
-import { useQuery } from '@tanstack/react-query'
-
 const Lesson = () => {
-  const {data} = useQuery({ queryFn: LessonService.get })
 
   return <div>Comming soon</div>
 }
