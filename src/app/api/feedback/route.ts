9ff01228a1744fc463d3aa19f2de9383bb93f695
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-11-18 13:18:20 +0700

    a

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,11 +1,10 @@
-import { firestore } from '@/service/firestore'
-import { addDoc, collection } from 'firebase/firestore'
-import { NextRequest, NextResponse } from 'next/server'
+import { firestore } from "@/service/firestore"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(request: NextRequest) {
   const payload = request.json()
   try {
-    const res = await firestore.collection('feedbacks').add(payload)
+    const res = await firestore.collection("feedbacks").add(payload)
     return new NextResponse(res.id)
   } catch (e) {
     return NextResponse.json(e)
