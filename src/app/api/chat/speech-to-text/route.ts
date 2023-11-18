0752976ec66ce7b1de65154c0a241a48551755ac
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2023-11-18 12:26:55 +0700

    a

--- src/app/api/chat/speech-to-text/route.ts
+++ src/app/api/chat/speech-to-text/route.ts
@@ -1,21 +1,21 @@
-import fs from 'fs'
-import path from 'path'
-import { openai } from '@/helpers/server-side/openai'
+import fs from "fs"
+import path from "path"
+import { openai } from "@/helpers/server-side/openai"
 
 export async function POST(req: any) {
   const form = await req.formData()
 
-  const blob = form.get('file')
+  const blob = form.get("file")
 
   if (!blob) {
-    return new Response('Bad Request', {
+    return new Response("Bad Request", {
       status: 400,
     })
   }
 
   const buffer = Buffer.from(await blob.arrayBuffer())
   const filename = `file.webm`
-  let filepath = `${path.join('public', 'uploads', filename)}`
+  let filepath = `${path.join("public", "uploads", filename)}`
 
   fs.writeFileSync(filepath, buffer)
 
@@ -23,25 +23,25 @@ export async function POST(req: any) {
   const stats: any = fs.statSync(filepath)
 
   if (parseInt(stats.size) < minFileSize) {
-    return new Response('Bad Request', {
+    return new Response("Bad Request", {
       status: 400,
     })
   }
   try {
-    const result: any = await openai.createTranscription(
-      fs.createReadStream(filepath) as any,
-      'whisper-1',
-      '',
-      'json',
-      0,
-      'en'
-    )
+    const result: any = await openai.audio.transcriptions.create({
+      file: fs.createReadStream(filepath) as any,
+      model: "whisper-1",
+      language: "en",
+      response_format: "json",
+      temperature: 0,
+      prompt: "transcrip correct content of audio",
+    })
 
     return new Response(result?.data?.text, {
       status: 200,
     })
   } catch (error: any) {
-    return new Response('', {
+    return new Response("", {
       status: 200,
     })
   }
