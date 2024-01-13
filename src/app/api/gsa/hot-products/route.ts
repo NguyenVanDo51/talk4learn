caa7247b2f7b20608d1d5e410f0c875c36de9b5f
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-01-13 18:20:16 +0700

    a

--- src/app/api/gsa/hot-products/route.ts
+++ src/app/api/gsa/hot-products/route.ts
@@ -2,8 +2,7 @@ import { appConfig, CACHE_DATA_TIMEOUT } from "@/libs/appConfig"
 
 export async function GET(req: Request) {
   const res = await fetch(
-    appConfig.apiDataBI +
-      "/hot_product/items/search" +
+    "https://gsa.bi.gobizdev.com/hot_product/items/search" +
       "?" +
       req.url.split("?")[1] || "",
     {
--- src/app/api/gsa/parent-categories/route.ts
+++ src/app/api/gsa/parent-categories/route.ts
@@ -1,11 +1,14 @@
 import { appConfig, CACHE_DATA_TIMEOUT } from "@/libs/appConfig"
 
 export async function GET() {
-  const res = await fetch(appConfig.apiDataBI + "/hot_product/get_cate_1", {
-    next: {
-      revalidate: CACHE_DATA_TIMEOUT,
-    },
-  })
+  const res = await fetch(
+    "https://gsa.bi.gobizdev.com/hot_product/get_cate_1",
+    {
+      next: {
+        revalidate: CACHE_DATA_TIMEOUT,
+      },
+    }
+  )
   const data = await res.json()
 
   return Response.json(data)
