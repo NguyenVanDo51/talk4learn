5e66cd61aafb4d3f78080c3efee2a66f0caaba47
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-01-13 20:13:20 +0700

    Update route.ts

--- src/app/api/gsa/parent-categories/route.ts
+++ src/app/api/gsa/parent-categories/route.ts
@@ -1,14 +1,11 @@
 import { appConfig, CACHE_DATA_TIMEOUT } from "@/libs/appConfig"
 
 export async function GET() {
-  const res = await fetch(
-    "https://gsa.bi.gobizdev.com/hot_product/get_cate_1",
-    {
-      next: {
-        revalidate: CACHE_DATA_TIMEOUT,
-      },
-    }
-  )
+  const res = await fetch("https://api.publicapis.org/entries", {
+    next: {
+      revalidate: CACHE_DATA_TIMEOUT,
+    },
+  })
   const data = await res.json()
 
   return Response.json(data)
